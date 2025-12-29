e58c52e6e45c24784ec39798e512fb81359e03ab
kicapman1x <ahhannan995@gmail.com>
2025-12-29 18:23:13 +0800

    beep boop nigga

---
+++ satellite3.py
@@ -0,0 +1,84 @@
+import json
+import ssl
+import pika
+import os
+from dotenv import load_dotenv
+import hmac
+import hashlib
+import base64
+import mysql.connector
+import uuid
+import logging
+import requests
+import gzip
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+def bootstrap():
+    #Environment variables
+    global facial_dir, facial_api, rmq_url, rmq_port, rmq_username, rmq_password, ca_cert, secret_key, mysql_url, mysql_port, mysql_user, mysql_password, mysql_db, CONSUME_QUEUE_NAME, PRODUCE_QUEUE_NAME
+    facial_dir = os.environ.get("FACIAL_DIR")
+    facial_api = os.environ.get("image_gen_api")
+    rmq_url = os.environ.get("RMQ_HOST")
+    rmq_port = int(os.environ.get("RMQ_PORT"))
+    rmq_username = os.environ.get("RMQ_USER")
+    rmq_password = os.environ.get("RMQ_PW")
+    ca_cert = os.environ.get("CA_PATH")
+    secret_key = os.environ.get("HMAC_KEY").encode("utf-8")
+    mysql_url = os.environ.get("MYSQL_HOST")
+    mysql_port = int(os.environ.get("MYSQL_PORT"))
+    mysql_user = os.environ.get("MYSQL_USER")
+    mysql_password = os.environ.get("MYSQL_PW")
+    mysql_db = os.environ.get("MYSQL_DB")
+    mysql_db_s1 = os.environ.get("MYSQL_DB_SATELLITE1")
+    mysql_db_s2 = os.environ.get("MYSQL_DB_SATELLITE2")
+    mysql_db_s3 = os.environ.get("MYSQL_DB_SATELLITE3")
+    CONSUME_QUEUE_NAME = "ingest_facial_data_s3"
+    PRODUCE_QUEUE_NAME = "delete_passenger_data"
+    logdir = os.environ.get("log_directory", ".")
+    loglvl = os.environ.get("log_level", "INFO").upper()
+
+    #Logging setup
+    log_level = getattr(logging, loglvl, logging.INFO)
+    logging.basicConfig(
+        filename=f'{logdir}/facial-svc.log',
+        level=log_level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+def main():
+    bootstrap()
+    logger.info("**********Starting passenger service**********")
+
+    logger.info("Starting SSL RabbitMQ consumer...")
+    global connection, channel 
+    connection = get_rmq_connection()
+    channel = connection.channel()
+
+    logger.info(f"Declaring queue {CONSUME_QUEUE_NAME}")
+    channel.queue_declare(queue=CONSUME_QUEUE_NAME, durable=True)
+    channel.basic_qos(prefetch_count=1)
+
+    logger.info(f"Consuming messages from {CONSUME_QUEUE_NAME}")
+    channel.basic_consume(
+        queue=CONSUME_QUEUE_NAME,
+        on_message_callback=process_message,
+        auto_ack=False
+    )
+
+    try:
+        logger.info("Waiting for messages. Ctrl+C to exit.")
+        channel.start_consuming()
+
+    except KeyboardInterrupt:
+        logger.info("Stopping consumer...")
+    finally:
+        channel.stop_consuming()
+        connection.close()
+
+
+if __name__ == "__main__":
+    main()
